3bdeefca0b956041d8eb75f1fc40ffb9c832106d
shruthimd <shruthi.d@coppermobile.com>
2015-11-17 13:07:13 -0600

    Initial Commit

---
+++ jj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jj
+//
+//  Created by Shruthi MD  on 11/17/15.
+//  Copyright © 2015 Shruthi MD . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
